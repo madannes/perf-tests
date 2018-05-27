2a29f107d36f7c60fb9586cb16b542b55f715f03
madannes <mad@nnes.net>
2018-05-27 18:46:38 +0000

    Copied everything over from the other repo

---
+++ perf-tests/Controllers/TestController.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.Entity;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace MvcPerfTest.Controllers
+{
+    public class TestController : Controller
+    {
+        private EfModel db = new EfModel();
+
+        public async Task<ActionResult> Page1(int? id)
+        {
+            var model = id != null ? await db.Page1Models.FindAsync(id) : new Page1Model { };
+            if (model == null) return HttpNotFound();
+            return View(model);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<ActionResult> Page1(Page1Model model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.Id != default(int))
+                    db.Entry(model).State = EntityState.Modified;
+                else
+                    db.Page1Models.Add(model);
+
+                await db.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ perf-tests/EfModel.cs
@@ -0,0 +1,15 @@
+namespace MvcPerfTest
+{
+    using System.Data.Entity;
+
+    public class EfModel : DbContext
+    {
+        public EfModel() : base("name=EfModel") { }
+
+        public virtual DbSet<Page1Model> Page1Models { get; set; }
+        public virtual DbSet<Page2Model> Page2Models { get; set; }
+        public virtual DbSet<Page3Model> Page3Models { get; set; }
+        public virtual DbSet<Page4Model> Page4Models { get; set; }
+        public virtual DbSet<Page5Model> Page5Models { get; set; }
+    }
+}
